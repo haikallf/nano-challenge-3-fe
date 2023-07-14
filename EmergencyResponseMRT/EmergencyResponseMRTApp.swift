658ed0abb6827d40cbe3d02a716978d60f133365
haikallf <haikallzrd@gmail.com>
2023-07-14 15:09:37 +0700

    init

---
+++ EmergencyResponseMRT/EmergencyResponseMRTApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmergencyResponseMRTApp.swift
+//  EmergencyResponseMRT
+//
+//  Created by Haikal Lazuardi Fadil on 14/07/23.
+//
+
+import SwiftUI
+
+@main
+struct EmergencyResponseMRTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
