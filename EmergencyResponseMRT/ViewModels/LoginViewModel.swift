e3c666dd4ae54e8be47921c60007e4f624417871
haikallf <haikallzrd@gmail.com>
2023-07-20 17:40:11 +0700

    update login viewmodel

--- EmergencyResponseMRT/ViewModels/LoginViewModel.swift
+++ EmergencyResponseMRT/ViewModels/LoginViewModel.swift
@@ -13,7 +13,39 @@ class LoginViewModel: ObservableObject {
     @Published var password: String = ""
     @Published var isAdmin: Bool = false
     
+    let baseURL = "https://c13f-158-140-189-122.ngrok-free.app"
+    
     func login() {
+        
+        let body: [String: Any] = ["email": email, "password": password]
+            
+        let jsonData = try? JSONSerialization.data(withJSONObject: body)
+        
+        guard let url = URL(string: "\(baseURL)/auth/user") else { return print("URL not found!") }
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+                request.setValue("\(String(describing: jsonData?.count))", forHTTPHeaderField: "Content-Length")
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            print("-----> data: \(String(describing: data))")
+            print("-----> error: \(String(describing: error?.localizedDescription))")
+            
+            guard let data = data, error == nil else {
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            print("-----1> responseJSON: \(responseJSON)")
+            if let responseJSON = responseJSON as? [String: Any] {
+                print("-----2> responseJSON: \(responseJSON)")
+            }
+        }
+        
+        task.resume()
         UserDefaults.standard.set(email, forKey: "email")
         UserDefaults.standard.set(password, forKey: "password")
         UserDefaults.standard.set(isAdmin, forKey: "isAdmin")
