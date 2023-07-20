2881fb6ba808bb1f7677af357d261dd2f7cff602
haikallf <haikallzrd@gmail.com>
2023-07-20 18:00:20 +0700

    add global states

---
+++ EmergencyResponseMRT/ViewModels/GlobalStates.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalStates.swift
+//  EmergencyResponseMRT
+//
+//  Created by Haikal Lazuardi Fadil on 20/07/23.
+//
+
+import SwiftUI
+
+class GlobalStates: ObservableObject {
+    @Published var baseURL = "https://c13f-158-140-189-122.ngrok-free.app"
+}
