9398fd91f7cc7e14ac0bf0c0735a497edcf95a63
haikallf <haikallzrd@gmail.com>
2023-07-21 17:58:54 +0700

    working socket and update list card

--- EmergencyResponseMRT/Views/ListView.swift
+++ EmergencyResponseMRT/Views/ListView.swift
@@ -6,33 +6,33 @@
 //
 
 import SwiftUI
+import WrapLayout
 
 struct ListView: View {
+    @State private var selectedItem = 0
     var body: some View {
-        ScrollView {
-            VStack {
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
-                
-                PassengerCard()
+        VStack {
+            ScrollView {
+                VStack {
+                    Picker("", selection: $selectedItem) {
+                        Text("Not Started").tag(0)
+                        Text("On Progress").tag(1)
+                        Text("Done").tag(2)
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    WrapLayout {
+                        PassengerCard()
+                        
+                        PassengerCard()
+                        
+                        PassengerCard()
+                    }
+                }
+                .padding(.bottom, 72)
+                .padding(.horizontal)
             }
-            .padding(.bottom, 72)
-            .padding(.horizontal)
+            .navigationTitle("Emergency")
         }
     }
 }
