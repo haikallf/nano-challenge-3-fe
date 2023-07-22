dabce5468f255dc8cefa9f2b0298db3425c4d3b4
haikallf <haikallzrd@gmail.com>
2023-07-23 02:14:48 +0700

    fix input field bug and add accent color

--- EmergencyResponseMRT/Views/LoginView.swift
+++ EmergencyResponseMRT/Views/LoginView.swift
@@ -13,66 +13,63 @@ struct LoginView: View {
     let isAdmin: Bool
     
     var body: some View {
-        ZStack {
-            VStack {
-                // MARK:- Page Logo
-               Image("SRS-Logo")
-                
-                Spacer()
-            }
-            .padding(.top, 66)
+        VStack(spacing: 14) {
+            // MARK:- Page Logo
+            Image("SRS-Logo")
+                .padding(.top, 66)
             
-            VStack(spacing: 14) {
-                // MARK:- Email Field
-                HStack(spacing: 14) {
-                    Image(systemName: "person.circle.fill")
-                        .resizable()
-                        .frame(width: 24, height: 24)
-                        .foregroundColor(.gray)
-                    
-                    TextField("Email", text: $viewModel.email)
-                        .font(.headline)
-                        .textInputAutocapitalization(.never)
-                }
-                .padding(.vertical)
-                .padding(.horizontal, 12)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.gray)
-                }
+            // MARK:- Email Field
+            HStack(spacing: 14) {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .foregroundColor(.gray)
                 
-                // MARK:- Password Field
-                HStack(spacing: 14) {
-                    Image(systemName: "lock.fill")
-                        .resizable()
-                        .frame(width: 17, height: 24)
-                        .foregroundColor(.gray)
-                    
-                    SecureField("Password", text: $viewModel.password)
-                        .font(.headline)
-                        .textInputAutocapitalization(.never)
-                }
-                .padding(.vertical)
-                .padding(.horizontal, 12)
-                .overlay {
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.gray)
-                }
+                TextField("Email", text: $viewModel.email)
+                    .font(.headline)
+                    .textInputAutocapitalization(.never)
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 12)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray)
+            }
+            .padding(.top, 61)
+            
+            // MARK:- Password Field
+            HStack(spacing: 14) {
+                Image(systemName: "lock.fill")
+                    .resizable()
+                    .frame(width: 17, height: 24)
+                    .foregroundColor(.gray)
                 
-                // MARK:- Login Button
-                CupertinoButton("LOGIN", action: {
-                    viewModel.isAdmin = isAdmin
-                    viewModel.login()
-                })
+                SecureField("Password", text: $viewModel.password)
+                    .font(.headline)
+                    .textInputAutocapitalization(.never)
+            }
+            .padding(.vertical)
+            .padding(.horizontal, 12)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray)
             }
             
-            //MARK: Navigate to TransitionView triggered by shouldNavigate
-            NavigationLink(destination: TransitionView(), isActive: $viewModel.shouldNavigate) {
-                EmptyView()
-            }
-            .opacity(0)
+            // MARK:- Login Button
+            CupertinoButton("LOGIN", action: {
+                viewModel.isAdmin = isAdmin
+                viewModel.login()
+            })
+            
+            Spacer()
         }
         .padding()
+        
+        //MARK: Navigate to TransitionView triggered by shouldNavigate
+        NavigationLink(destination: TransitionView(), isActive: $viewModel.shouldNavigate) {
+            EmptyView()
+        }
+        .opacity(0)
     }
 }
 
